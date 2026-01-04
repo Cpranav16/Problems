6c04d5cd109a4d450311c98ff7a6bbf92287c632
Pranav Chavan <110090621+Cpranav16@users.noreply.github.com>
2026-01-05 00:16:44 +0530

    Add 1480. Running Sum of 1D Array solution

--- arrays/1480_Running_Sum_of_1D_Array.java
+++ arrays/1480_Running_Sum_of_1D_Array.java
@@ -1,3 +1,7 @@
+// LeetCode 1480: Running Sum of 1D Array
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
 class Solution {
     public int[] runningSum(int[] nums) {
         for (int i = 1; i < nums.length; i++) {
