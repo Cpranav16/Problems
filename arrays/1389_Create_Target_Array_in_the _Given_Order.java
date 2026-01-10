4f70662c204dd324b3d4ac4d5d8368cf5a541804
Pranav Chavan <110090621+Cpranav16@users.noreply.github.com>
2026-01-10 22:11:15 +0530

    Add solution for creating target array
    Implement solution for creating target array based on given order.

---
+++ arrays/1389_Create_Target_Array_in_the _Given_Order.java
@@ -0,0 +1,20 @@
+// Leetcode 1389. Create Target Array in the Given Order
+// Time Complexity: O(n²)
+// Space Complexity: O(n)
+
+class Solution {
+    public int[] createTargetArray(int[] nums, int[] index) {
+        List<Integer> target = new ArrayList<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            target.add(index[i], nums[i]);
+        }
+
+        int[] result = new int[target.size()];
+        for (int i = 0; i < target.size(); i++) {
+            result[i] = target.get(i);
+        }
+
+        return result;
+    }
+}
