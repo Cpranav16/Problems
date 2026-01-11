7781e4a99b6a5e952c387452c4016da32bb3282f
Pranav Chavan <110090621+Cpranav16@users.noreply.github.com>
2026-01-12 00:51:43 +0530

    Add comments for complexity analysis in solution
    Added comments for time and space complexity.

---
+++ arrays/1773_Count_Items_Matching_a_Rule.java
@@ -0,0 +1,24 @@
+// Leetcode : 1773. Count Items Matching a Rule
+// Time Complexity: O(n)
+// Spcae Complexity: O(1)
+
+class Solution {
+    public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
+        int index = 0; // index to check in each item
+        if (ruleKey.equals("type")) {
+            index = 0;
+        } else if (ruleKey.equals("color")) {
+            index = 1;
+        } else if (ruleKey.equals("name")) {
+            index = 2;
+        }
+
+        int count = 0;
+        for (List<String> item : items) {
+            if (item.get(index).equals(ruleValue)) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
