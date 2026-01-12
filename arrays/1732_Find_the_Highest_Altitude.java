0b9145af32d78a1dbd72b690dfef83494fd95fcd
Pranav Chavan <110090621+Cpranav16@users.noreply.github.com>
2026-01-12 22:30:11 +0530

    Add solution for finding highest altitude
    Implement solution for finding the highest altitude based on altitude gains.

---
+++ arrays/1732_Find_the_Highest_Altitude.java
@@ -0,0 +1,17 @@
+// Leetcode 1732. Find the Highest Altitude
+// Time complexity: O(n)
+// Space complexity: O(1)
+
+class Solution {
+    public int largestAltitude(int[] gain) {
+        int altitude = 0;
+        int maxAltitude = 0;
+
+        for (int g : gain) {
+            altitude += g;
+            maxAltitude = Math.max(maxAltitude, altitude);
+        }
+
+        return maxAltitude;
+    }
+}
